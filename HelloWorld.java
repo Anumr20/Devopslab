265af363f019d1ea356b9848bfcb2cfeac55fec3
Ananya M R <ananyamr20@gmail.com>
2025-10-16 10:06:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ Create HelloWorld.java with:
 package com.example;
 public class HelloWorld {
 public static void main(String[] args) {
-System.out.println("Hello, DevOps CI/CD using GitHub Actions!");
+System.out.println("HDevOps CI/CD using GitHub Actions!");
 }
 }
